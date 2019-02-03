b03a2546190aa463ab25ddbb168406bee9a1bd82
Jake Witcher <jjwitcher09@gmail.com>
2019-02-03 18:12:14 -0500

    refactor Player components

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -1,131 +1,64 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
-import url from '../utils/const';
-import { formatName } from '../utils/helpers';
 
-class PlayerForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      rating: null,
-      handedness: 'Left',
-      error: '',
-    };
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleValidation = this.handleValidation.bind(this);
-  }
-
-  handleInput(event) {
-    const { name, value } = event.target;
-    if (name === 'rating') {
-      if (!value || value.match(/^[0-9]\d*$/)) {
-        this.setState({
-          [name]: value,
-        });
-      }
-    } else {
-      this.setState({
-        [name]: value,
-      });
-    }
-  }
-
-
-  handleSubmit(event) {
-    event.preventDefault();
-    if (this.handleValidation()) {
-      const data = JSON.stringify({
-        first_name: formatName(this.state.firstName),
-        last_name: formatName(this.state.lastName),
-        rating: parseInt(this.state.rating, 10),
-        handedness: this.state.handedness,
-      });
-      const token = sessionStorage.getItem('token');
-      axios({
-        method: 'post',
-        url: `${url}/players`,
-        data,
-        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
-      })
-        .then(() => {
-          this.setState({
-            firstName: '',
-            lastName: '',
-            handedness: 'Left',
-            error: '',
-          });
-          this.props.navigate('../../roster');
-        })
-        .catch(error => {
-          if (error) {
-            this.setState({
-              error: 'We\'re having trouble creating a new player. This is either a problem with our servers or your internet connection. Please try again later.',
-            });
-          }
-        });
-    }
-  }
-
-  handleValidation() {
-    if (!this.state.firstName || !this.state.lastName) {
-      this.setState({
-        error: 'Please enter both a first and last name',
-      });
-      return false;
-    } else if (!this.state.rating) {
-      this.setState({
-        error: 'Please enter a rating',
-      });
-      return false;
-    }
-    this.setState({
-      error: '',
-    });
-    return true;
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Register form</h2>
-        {!this.state.error || <p>{this.state.error}</p>}
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="firstName">
-            First Name
-            <input type="text" id="firstName" value={this.state.firstName} name="firstName" onChange={this.handleInput} placeholder="first name" />
-          </label>
-          <label htmlFor="lastName">
-            Last Name
-            <input type="text" id="lastName" value={this.state.lastName} name="lastName" onChange={this.handleInput} placeholder="last name" />
-          </label>
-          <label htmlFor="rating">
-            Rating
-            <input type="text" id="rating" value={this.state.rating} name="rating" onChange={this.handleInput} placeholder="rating" />
-          </label>
-          <label htmlFor="handedness">
-            Handedness
-            <select id="handedness" value={this.state.handedness} name="handedness" onChange={this.handleInput}>
-              <option value="Left">Left</option>
-              <option value="Right">Right</option>
-            </select>
-          </label>
-          <input type="submit" id="create" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
+const PlayerForm = props => {
+  const {
+    firstName,
+    lastName,
+    rating,
+    handedness,
+    error,
+    handleInput,
+    handleSubmit,
+  } = props;
+  return (
+    <div>
+      <h2>Register form</h2>
+      {!error || <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">
+          First Name
+          <input type="text" id="firstName" value={firstName} name="firstName" onChange={handleInput} placeholder="first name" />
+        </label>
+        <label htmlFor="lastName">
+          Last Name
+          <input type="text" id="lastName" value={lastName} name="lastName" onChange={handleInput} placeholder="last name" />
+        </label>
+        <label htmlFor="rating">
+          Rating
+          <input type="text" id="rating" value={rating} name="rating" onChange={handleInput} placeholder="rating" />
+        </label>
+        <label htmlFor="handedness">
+          Handedness
+          <select id="handedness" value={handedness} name="handedness" onChange={handleInput}>
+            <option value="Left">Left</option>
+            <option value="Right">Right</option>
+          </select>
+        </label>
+        <input type="submit" id="create" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default PlayerForm;
 
 PlayerForm.propTypes = {
-  navigate: PropTypes.func,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  rating: PropTypes.number,
+  handedness: PropTypes.string,
+  error: PropTypes.string,
+  handleInput: PropTypes.func,
+  handleSubmit: PropTypes.func,
 };
 
 PlayerForm.defaultProps = {
-  navigate: () => {},
+  firstName: '',
+  lastName: '',
+  rating: null,
+  handedness: '',
+  error: '',
+  handleInput: () => {},
+  handleSubmit: () => {},
 };
--- src/containers/PlayerPage.jsx
+++ src/containers/PlayerPage.jsx
@@ -1,12 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import url from '../utils/const';
+import { formatName } from '../utils/helpers';
 
 import PlayerForm from '../components/PlayerForm';
 
-const PlayerPage = props => (
-  <div>
-    <h1>Player page</h1>
-    <PlayerForm {...props} />
-  </div>
-);
+class PlayerPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      rating: null,
+      handedness: 'Left',
+      error: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
+  }
+
+  handleInput(event) {
+    const { name, value } = event.target;
+    if (name === 'rating') {
+      if (!value || value.match(/^[0-9]\d*$/)) {
+        this.setState({
+          [name]: value,
+        });
+      }
+    } else {
+      this.setState({
+        [name]: value,
+      });
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.handleValidation()) {
+      const data = JSON.stringify({
+        first_name: formatName(this.state.firstName),
+        last_name: formatName(this.state.lastName),
+        rating: parseInt(this.state.rating, 10),
+        handedness: this.state.handedness,
+      });
+      const token = sessionStorage.getItem('token');
+      axios({
+        method: 'post',
+        url: `${url}/players`,
+        data,
+        headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'application/json' },
+      })
+        .then(() => {
+          this.setState({
+            firstName: '',
+            lastName: '',
+            handedness: 'Left',
+            error: '',
+          });
+          this.props.navigate('../../roster');
+        })
+        .catch(error => {
+          if (error) {
+            this.setState({
+              error: 'We\'re having trouble creating a new player. This is either a problem with our servers or your internet connection. Please try again later.',
+            });
+          }
+        });
+    }
+  }
+
+  handleValidation() {
+    if (!this.state.firstName || !this.state.lastName) {
+      this.setState({
+        error: 'Please enter both a first and last name',
+      });
+      return false;
+    } else if (!this.state.rating) {
+      this.setState({
+        error: 'Please enter a rating',
+      });
+      return false;
+    }
+    this.setState({
+      error: '',
+    });
+    return true;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Player page</h1>
+        <PlayerForm
+          firstName={this.state.firstName}
+          lastName={this.state.lastName}
+          rating={this.state.rating}
+          handedness={this.state.handedness}
+          error={this.state.error}
+          handleInput={this.handleInput}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
 
 export default PlayerPage;
+
+PlayerPage.propTypes = {
+  navigate: PropTypes.func,
+};
+
+PlayerPage.defaultProps = {
+  navigate: () => {},
+};
