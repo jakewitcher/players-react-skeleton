065ca0702e778a28f9fe0446ff86ff426c202ee9
Jake Witcher <jjwitcher09@gmail.com>
2019-02-06 22:21:20 -0500

    fix logout test, clear session storage prior to starting test

--- tests/e2e/integration/logout.spec.js
+++ tests/e2e/integration/logout.spec.js
@@ -1,5 +1,10 @@
 describe('Logout', () => {
   it('Logs out the user', () => {
+    cy.visit('/roster', {
+      onBeforeLoad: win => {
+        win.sessionStorage.clear();
+      },
+    });
     cy.login();
     cy.url().should('eq', 'http://localhost:3000/roster');
     cy.logout();
